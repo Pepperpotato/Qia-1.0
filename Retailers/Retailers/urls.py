86b27984a6584ce53bda84e4f0fbd2da417356e4
ddd <ddd@jdjdjd.com>
2019-06-29 15:50:34 +0800

    goods表提交

--- Retailers/Retailers/urls.py
+++ Retailers/Retailers/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
-    url(r'^app/', include('App.urls', namespace='app'))
+    url(r'^Goods/', include('Goods.urls', namespace='Goods'))
 ]
