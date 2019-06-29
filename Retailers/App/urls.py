6b7b631f3f5b8500e24cf18b120e9aeeb60c9918
Pepperpotato <815540246@qq.com>
2019-06-29 10:07:46 +0800

    新工程目录

--- Retailers/App/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-
-from App import views
-
-urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
-    url(r'^add/$', views.index, name='index')
-]
--- Retailers/App/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from App.models import User
-
-# Create your views here.
-
-
-def index(request):
-    return None
