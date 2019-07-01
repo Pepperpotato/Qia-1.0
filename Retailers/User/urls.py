97e97aef80436a15d4ab47fc5497ae5421b5e8b3
ddd <ddd@jdjdjd.com>
2019-07-01 22:20:01 +0800

    order

--- Retailers/User/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-
-from User import views
-
-urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
-    url(r'^add/$', views.add, name='add')
-]
