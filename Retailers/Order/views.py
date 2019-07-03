5bddfa66a08c412a2c60d64321220c419ffbe716
ddd <ddd@jdjdjd.com>
2019-07-03 15:28:16 +0800

    4

--- Retailers/Order/views.py
+++ Retailers/Order/views.py
@@ -12,10 +12,10 @@ from .models import *
 
 def home(request):
     temp = loader.get_template('shop/home/home3.html')
-    dlb = CommodityCategories.objects.filter(parentid=0) #寻找大类别
-    xlb = CommodityCategories.objects.exclude(parentid=0) #寻找小类别
-    store = CommodityBrand.objects.all()#寻找品牌
-    commodity =
+    dlb = CommodityCategories.objects.filter(parentid=0) #寻找大类别 3
+    xlb = CommodityCategories.objects.exclude(parentid=0) #寻找小类别 3
+    store = CommodityBrand.objects.all()#寻找品牌 2
+    commodity = dlb[0].
     res = temp.render(context={'dlb':dlb,'xlb':xlb,'store':store})
     return HttpResponse(res)
     # return render(request,'shop/home/home3.html')
