75ef5cc826035bf300786ee766803962a030db23
ddd <ddd@jdjdjd.com>
2019-07-03 10:19:05 +0800

    2

--- Retailers/Retailers/settings.py
+++ Retailers/Retailers/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'Order.middleware1.Middleware1',
 ]
 
 ROOT_URLCONF = 'Retailers.urls'
