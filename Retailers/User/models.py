0b83916d74aa7e2e018c5ee39798ab4f59e5d41d
Pepperpotato <815540246@qq.com>
2019-07-04 17:10:40 +0800

    修改字段

--- Retailers/User/models.py
+++ Retailers/User/models.py
@@ -19,14 +19,14 @@ class User(models.Model):
     certificate_id = models.CharField(max_length=20, null=True)    # 证件号码
     phone_number = models.CharField(max_length=20, null=True)  # 手机号码
     email = models.CharField(max_length=50, null=True)     # 邮箱
-    sex = models.CharField(choices=((1, "男"), (2, "女"), (0, "保密")), default=1, null=True, max_length=5)   # 性别
+    sex = models.CharField(choices=((1, "男"), (2, "女"), (0, "保密")), default=1, max_length=5)   # 性别
     shopping_grade = models.IntegerField(default=0, null=True)      # 购物积分
-    reg_time = models.DateTimeField(default=datetime.now, null=True)   # 注册时间
+    reg_time = models.DateTimeField(default=datetime.now)   # 注册时间
     birthday = models.CharField(max_length=20, null=True)   # 生日
-    user_photo = models.CharField(max_length=100, null=True, default='/admin/upload/avatar_blank.gif')    # 用户头像
-    user_status = models.IntegerField(default=0, null=True)     # 用户状态 0为正常,1为锁定
-    vip_level = models.IntegerField(default=0, null=True)   # 会员等级
-    safety_grade = models.IntegerField(default=0, null=True)     # 安全积分
+    user_photo = models.CharField(max_length=100, default='/admin/upload/avatar_blank.gif')    # 用户头像
+    user_status = models.IntegerField(default=0)     # 用户状态 0为正常,1为锁定
+    vip_level = models.IntegerField(default=0)   # 会员等级
+    safety_grade = models.IntegerField(default=0)     # 安全积分
     question1 = models.CharField(max_length=50, null=True)  # 安全问题1
     answer1 = models.CharField(max_length=50, null=True)    # 答案1
     question2 = models.CharField(max_length=50, null=True)  # 安全问题2
@@ -41,11 +41,11 @@ class User(models.Model):
 class User_grade(models.Model):
     gid = models.AutoField(primary_key=True)    # 用户积分表id
     uid = models.ForeignKey('User', models.CASCADE)     # 用户id
-    change_source = models.CharField(max_length=50)     # 积分变更来源
+    change_source = models.CharField(max_length=50, null=True)     # 积分变更来源
     change_number = models.IntegerField(null=True)     # 积分变更数值
     change_time = models.DateTimeField(default=datetime.now, null=True)     # 变更时间
-    changed_grade = models.IntegerField(null=True)     # 变更后积分
-    growth_value = models.IntegerField(null=True)      # 会员成长值
+    changed_grade = models.IntegerField(null=True, default=0)     # 变更后积分
+    growth_value = models.IntegerField(null=True, default=0)      # 会员成长值
 
     class Meta:
         db_table = 'user_grade'
@@ -56,10 +56,10 @@ class User_address(models.Model):
     aid = models.AutoField(primary_key=True)    # 地址id
     uid = models.ForeignKey('User', models.CASCADE)     # 用户id
     default_address = models.IntegerField(default=0)    # 是否默认地址,0为否，1为是
-    location = models.CharField(max_length=20)  # 所在地
+    location = models.CharField(max_length=20, null=True)  # 所在地
     detail_address = models.CharField(max_length=100, null=True)    # 默认地址
     receiver = models.CharField(max_length=10, null=True)   # 收货人
-    phone_number = models.CharField(max_length=11)      # 手机号
+    phone_number = models.CharField(max_length=11, null=True)      # 手机号
 
     class Meta:
         db_table = 'user_address'
@@ -81,7 +81,7 @@ class User_collection(models.Model):
     cid = models.AutoField(primary_key=True)    # 通知表id
     uid = models.ForeignKey('User', models.CASCADE)     # 用户id
     gid = models.IntegerField(null=True)
-    collection_time = models.DateTimeField(default=datetime.now, null=True)    # 收藏时间
+    collection_time = models.DateTimeField(default=datetime.now)    # 收藏时间
 
     class Meta:
         db_table = 'user_collection'
@@ -91,7 +91,7 @@ class User_collection(models.Model):
 class User_notice(models.Model):
     nid = models.AutoField(primary_key=True)    # 通知表id
     picture_path = models.CharField(max_length=200, null=True)    # 活动图片路径
-    sendtime = models.DateTimeField(default=datetime.now, null=True)    # 发送时间
+    sendtime = models.DateTimeField(default=datetime.now)    # 发送时间
     tag_path = models.CharField(max_length=200, null=True)  # 活动内容标签路径
 
     class Meta:
@@ -102,7 +102,7 @@ class User_notice(models.Model):
 class User_account(models.Model):
     # uid = models.ForeignKey('User', models.CASCADE)     # 用户id
     pay_password = models.CharField(max_length=128, null=True)     # 支付密码
-    bankcard_id = models.CharField(max_length=50,default=0, null=True)   # 银行卡号
+    bankcard_id = models.CharField(max_length=50, default=0)   # 银行卡号
     money = models.IntegerField(default=0, null=True)      # 账户余额
     alipay_number = models.CharField(max_length=20, null=True)  # 支付宝账号
     wechat_number = models.CharField(max_length=20, null=True)  # 微信账号
@@ -110,7 +110,7 @@ class User_account(models.Model):
     goodoffer_id = models.CharField(max_length=100, null=True)   # 商品优惠券id
     used_userofferid = models.CharField(max_length=100, null=True)  # 已使用用户优惠券id
     used_goodofferid = models.CharField(max_length=100, null=True)  # 已使用商品优惠券id
-    integral_id = models.IntegerField(null=True, default=1)    # 积分表id
+    integral_id = models.IntegerField(null=True)    # 积分表id
     user = models.OneToOneField(to='User', on_delete=models.CASCADE, db_column='uid')  # 用户id
 
     class Meta:
@@ -119,11 +119,11 @@ class User_account(models.Model):
 
 # 用户优惠券表
 class User_offer(models.Model):
-    offer_content = models.CharField(max_length=200)    # 优惠内容描述
+    offer_content = models.CharField(max_length=200, null=True)    # 优惠内容描述
     offer_time = models.DateTimeField(null=True)    # 优惠时间
     # status = models.IntegerField(default=0)   # 使用状态 0为未使用，1为使用
-    offer_door = models.IntegerField(default=100, null=True)   # 优惠门槛
-    offer_money = models.IntegerField(default=10)   # 优惠金额
+    offer_door = models.IntegerField(null=True)   # 优惠门槛
+    offer_money = models.IntegerField(null=True)   # 优惠金额
 
     class Meta:
         db_table = 'user_offer'
@@ -161,4 +161,5 @@ class Pay_way(models.Model):
 
     class Meta:
         db_table = 'pay_way'
+
 
